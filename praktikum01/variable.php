8b7bb143773f96f20698746d762018c23c0fdeac
Ahmad Waluyo <115085884+JoeAchmed@users.noreply.github.com>
2023-03-25 13:10:50 +0700

    Praktikum Web 01

---
+++ praktikum01/variable.php
@@ -0,0 +1,15 @@
+
+<?php
+
+echo "hello world";
+echo "<br>";
+
+// buat variable
+$nama = "Ahmad Waluyo";
+echo $nama . "<br>";
+$jurusan = "Teknik Informatika";
+$semester = 2;
+
+echo "saya kuliah di jurusan " . $jurusan . "<br>";
+
+?>
